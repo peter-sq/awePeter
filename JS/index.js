6fd7c7cbee88c9b8486a5f61f4a31d6eeda6cd42
peter-sq <awe.petersq@gmail.com>
2021-06-11 21:31:30 +0100

    added all files

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+function submitData(){
+    var firstName = document.getElementById('firstname').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('Phone').value;
+    alert("Your first name is "+firstName+". Your e-mail is "+email+". and phone number is "+phone);
+
+    
+}
